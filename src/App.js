178bf995cecabcae9db6f22b2c2074271b854d9e
chupacambra <jovankoul@hotmail.com>
2023-09-09 20:51:19 +0300

    Added Mocks for async functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import './App.css';
 import Login from './components/login/Login';
-
 function App() {
-  const a = 1;
-  const b = 2;
   return (
     <div className='App'>
-      <h1 data-testid='mytestid'>Hello</h1>
-      <ul>
-        <li>Banana</li>
-        <li>Orange</li>
-        <li>Apple</li>
-      </ul>
-      <span title='sum'>{a + b}</span>
-      {/* <Login /> */}
+      <Login />
     </div>
   );
 }
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,11 +1,11 @@
-import axios from "axios";
-import { useState } from "react";
+import axios from 'axios';
+import { useState } from 'react';
 
 const Login = () => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [user, setUser] = useState({});
 
   const handleClick = async (e) => {
@@ -13,38 +13,37 @@ const Login = () => {
     setLoading(true);
     try {
       const { data } = await axios.get(
-        "https://jsonplaceholder.typicode.com/users/1"
+        'https://jsonplaceholder.typicode.com/users/1'
       );
       setUser(data);
     } catch {
       setError(true);
     }
-    setLoading(false)
+    setLoading(false);
   };
 
   return (
-    <div className="container">
-      <h1>Learn React</h1>
-      <span className="user">{user.name}</span>
+    <div className='container'>
+      <span className='user'>{user.name}</span>
       <form>
         <input
-          type="text"
-          placeholder="username"
+          type='text'
+          placeholder='username'
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
         <input
-          type="password"
-          placeholder="password"
+          type='password'
+          placeholder='password'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
         <button disabled={!username || !password} onClick={handleClick}>
-          {loading ? "please wait" : "Login"}
+          {loading ? 'please wait' : 'Login'}
         </button>
         <span
-          data-testid="error"
-          style={{ visibility: error ? "visible" : "hidden" }}
+          data-testid='error'
+          style={{ visibility: error ? 'visible' : 'hidden' }}
         >
           Something went wrong!
         </span>
